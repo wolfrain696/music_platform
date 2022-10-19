0655e324cb974624b128aafbc6b5a8cfd51aa090
HP <wolfrain696@gmail.com>
2022-10-19 17:22:27 +0500

    fix file

--- client/pages/tracks.tsx
+++ client/pages/tracks.tsx
@@ -1,18 +1,18 @@
-import MainLayout from '../../layouts/MainLayout';
+import MainLayout from '../layouts/MainLayout';
 import { Card, Grid, Button } from '@mui/material';
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-import { TracksList } from '../../components/TrackList/TracksList';
-import { useAppSelector } from '../../hooks/redux-hooks';
-import { fetchTracks } from '../../store/asyncThunks/fetchTracks';
-import { AppThunk, wrapper } from '../../store';
+import { TracksList } from '../components/TrackList/TracksList';
+import { useAppSelector } from '../hooks/redux-hooks';
+import { fetchTracks } from '../store/asyncThunks/fetchTracks';
+import { AppThunk, wrapper } from '../store';
 
 const StyledCard = styled(Card)`
   width: 900px;
   padding: 0 16px;
 `;
 
-export default function Index() {
+export default function Tracks() {
   const router = useRouter();
   const { error } = useAppSelector((state) => state.tracks);
   const handleGoCreateTrackPage = () => {
